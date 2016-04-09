12e9c34d4f55f64f3a3c1d62d50c0dfa5a55d379
Hiroaki Kadomatsu <note103@gmail.com>
2016-04-09 19:20:19 +0900

    Add a script

---
+++ dirmove.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $arg = shift;
+$arg = 1 unless $arg;
+
+my $dir = '.';
+my $pwd = `pwd`;
+
+main($pwd, $dir, $arg);
+
+sub main {
+    ($pwd, $dir, $arg) = @_;
+    chomp($pwd);
+
+    # 選択（peco/sentaku）
+    my $selector = '';
+    if ($arg eq 's') {
+        $selector = 'sentaku'
+    } else {
+        $selector = 'peco'
+    }
+
+    $dir = `if [ -n "\$( ls -F "$pwd" | grep / )" ]; then ( ls -F "$pwd" | grep / | "$selector" ) else echo ''; fi 2>/dev/null`;
+
+    # ディレクトリが無ければ（＝改行が戻り）脱出
+    out($pwd) if ($dir =~ /\A\n\z/);
+
+    # 移動中断したら（＝空文字が戻り）脱出
+    if ($dir eq '') {
+        out($pwd);
+    } else {
+        $dir =~ s/\/$//;
+        $pwd = "$pwd/$dir";
+        main($pwd, $dir, $arg);
+    }
+}
+
+sub out {
+    $pwd = shift;
+    print `echo $pwd`;
+    exit;
+}
