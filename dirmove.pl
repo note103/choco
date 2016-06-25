88badaba97fe43442d804d7877985c0bde9f671c
Hiroaki Kadomatsu <note103@gmail.com>
2016-06-25 18:10:44 +0900

    Update dirmove.pl
    feature cho

--- dirmove.pl
+++ dirmove.pl
@@ -5,78 +5,91 @@ use feature 'say';
 use Time::Piece;
 
 my $args = shift;
-
-$args = 1 unless $args;
 my @args = split /\s+/, $args;
 
-my $attr = {};
-$attr->{option} = 1;
-$attr->{selector} = 1;
+my $back     = 1;
+my $fmt      = '';
+my $option   = '-F';
+my $selector = 'cho';
+my $pwd = `pwd`; chomp $pwd; $pwd = $pwd.'/';
 
 for (@args) {
-    if ($_ eq 's') {
-        $attr->{selector} = 's';
-    } elsif ($_ eq '.') {
-        $attr->{option} = '.';
+    chomp $_;
+    if    ( $_ =~ /d/)  { $fmt = 'd'; }
+    elsif ( $_ =~ /f/)  { $fmt = 'f'; }
+    elsif ( $_ =~ /p/)  { $selector = 'peco'; }
+    elsif ( $_ =~ /\./) {
+        $back = 0;
+        $option = '-aF'; }
+    else                {
+        out($pwd);
     }
 }
 
 my $dir = '.';
-my $pwd = `pwd`;
+my $quit   = 'exit';
+my $epoch = localtime->epoch;
 
-main($pwd, $dir, $args);
+main($pwd, $dir, $args, $fmt);
 
 sub main {
-    ($pwd, $dir, $args) = @_;
+    ($pwd, $dir, $args, $fmt) = @_;
     chomp($pwd);
 
-    my $selector = '';
-    if ($attr->{selector} eq 's') {
-        $selector = 'sentaku'
+    my $parent;
+    if ( $back == 1 ) {
+        $parent = "../";
     } else {
-        $selector = 'peco'
+        $parent = "";
     }
 
-    # 不可視チェック
-    my $back = '';
-    my $option = '';
-
-    if ($attr->{option} eq '.') {
-        $option = '-aF';
+    if ($fmt eq 'd' ) {
+        $dir = `
+            if [ -n "\$( ls "$option" "$pwd" | grep -e / -e @ )" ]; then
+                tmp=\$(ls "$option" "$pwd" | grep -e / -e @ | sed 's/ /$epoch/g');
+                for i in $quit $parent \$tmp ; do echo \$i | sed 's/$epoch/ /g'; done | $selector
+            else
+                for i in $quit $parent ; do echo \$i; done | $selector
+            fi 2>/dev/null
+        `;
+    }
+    elsif ($fmt eq 'f' ) {
+        $dir = `
+            if [ -n "\$( ls "$option" "$pwd" | grep -e / )" ]; then
+                tmp=\$(ls "$option" "$pwd" | grep -v / | sed 's/ /$epoch/g');
+            fi 2>/dev/null
+            for i in $quit $parent \$tmp ; do echo \$i | sed 's/$epoch/ /g'; done | $selector
+        `;
     } else {
-        $option = '-F';
-        $back = "../\n";
+        $dir = `
+            tmp=\$(ls "$option" "$pwd" | grep -e / | sed 's/ /$epoch/g')
+            rest=\$(ls "$option" "$pwd" | grep -v / | sed 's/ /$epoch/g')
+            for i in $quit $parent \$tmp \$rest ; do echo \$i | sed 's/$epoch/ /g'; done | $selector
+        `;
     }
 
-    my $tmp = localtime->datetime(T=>'-');
-    $tmp =~ s/:/-/g;
-
-    $dir = `
-    if [ -n "\$( ls "$option" "$pwd" | grep / )" ]; then
-        echo "$back" > $tmp;
-        ls "$option" "$pwd" | grep / >> $tmp;
-        cat $tmp | "$selector";
-        rm $tmp;
-    else
-        echo "$back" | "$selector"
-    fi 2>/dev/null
-    `;
-
-    # ディレクトリが無ければ（＝改行が戻り）脱出
-    out($pwd) if ($dir =~ /\A\n\z/);
-
-    # 移動中断したら（＝空文字が戻り）脱出
-    if ($dir eq '') {
+    chomp $dir;
+    if ($dir =~ /(.+)( \/|\@ )\z/x) {
+        my $base = $1;
+        $dir =~ s/@\z/\//;
+        $pwd = "$pwd$base/";
+        main($pwd, $dir, $args, $fmt);
+    }
+    elsif ($dir =~ /$quit/) {
         out($pwd);
-    } else {
-        $dir =~ s/\/$//;
-        $pwd = "$pwd/$dir";
-        main($pwd, $dir, $args);
+    }
+    elsif ($dir =~ /[^\/]$/) {
+        $pwd = "$pwd$dir";
+        out($pwd);
+    }
+    else {
+        out($pwd);
     }
 }
 
 sub out {
     $pwd = shift;
+    $pwd =~ s/@\z//;
     print `echo $pwd`;
     exit;
 }
