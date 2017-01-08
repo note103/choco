b3ca69ecbb17dc57947efab9946152d9d154349c
KADOMATSU Hiroaki <note103@gmail.com>
2017-01-08 12:03:22 +0900

    Improve overall

--- choco.pl
+++ choco.pl
@@ -4,23 +4,24 @@ use warnings;
 use feature 'say';
 use Time::Piece;
 
-my $args = shift;
-my @args = split /\s+/, $args;
+my @args = @ARGV;
 
 my $back     = 1;
-my $fmt      = '';
 my $option   = '-F';
 my $selector = 'cho';
 my $pwd = `pwd`; chomp $pwd; $pwd = $pwd.'/';
+my $command = 'echo';
 
 for (@args) {
     chomp $_;
-    if    ( $_ =~ /d/)  { $fmt = 'd'; }
-    elsif ( $_ =~ /f/)  { $fmt = 'f'; }
-    elsif ( $_ =~ /p/)  { $selector = 'peco'; }
+    if ( $_ =~ /p/)  { $selector = 'peco'; }
+    elsif ( $_ =~ /o/) {
+        $command = 'open';
+    }
     elsif ( $_ =~ /\./) {
         $back = 0;
-        $option = '-aF'; }
+        $option = '-aF';
+    }
     else                {
         out($pwd);
     }
@@ -30,10 +31,10 @@ my $dir = '.';
 my $quit   = 'exit';
 my $epoch = localtime->epoch;
 
-main($pwd, $dir, $args, $fmt);
+main($pwd, $dir, @args);
 
 sub main {
-    ($pwd, $dir, $args, $fmt) = @_;
+    ($pwd, $dir, @args) = @_;
     chomp($pwd);
 
     my $parent;
@@ -43,37 +44,26 @@ sub main {
         $parent = "";
     }
 
-    if ($fmt eq 'd' ) {
-        $dir = `
-            if [ -n "\$( ls "$option" "$pwd" | grep -e / -e @ )" ]; then
-                tmp=\$(ls "$option" "$pwd" | grep -e / -e @ | sed 's/ /$epoch/g');
-                for i in $quit $parent \$tmp ; do echo \$i | sed 's/$epoch/ /g'; done | $selector
-            else
-                for i in $quit $parent ; do echo \$i; done | $selector
-            fi 2>/dev/null
-        `;
-    }
-    elsif ($fmt eq 'f' ) {
-        $dir = `
-            if [ -n "\$( ls "$option" "$pwd" | grep -e / )" ]; then
-                tmp=\$(ls "$option" "$pwd" | grep -v / | sed 's/ /$epoch/g');
-            fi 2>/dev/null
-            for i in $quit $parent \$tmp ; do echo \$i | sed 's/$epoch/ /g'; done | $selector
-        `;
-    } else {
-        $dir = `
-            tmp=\$(ls "$option" "$pwd" | grep -e / | sed 's/ /$epoch/g')
-            rest=\$(ls "$option" "$pwd" | grep -v / | sed 's/ /$epoch/g')
-            for i in $quit $parent \$tmp \$rest ; do echo \$i | sed 's/$epoch/ /g'; done | $selector
-        `;
-    }
+    $dir = `
+        tmp=\$(ls "$option" "$pwd" | grep -e / | sed 's/ /$epoch/g')
+        rest=\$(ls "$option" "$pwd" | grep -v / | sed 's/ /$epoch/g')
+        for i in $quit $parent \$tmp \$rest ; do echo \$i | sed 's/$epoch/ /g'; done | $selector
+    `;
 
     chomp $dir;
-    if ($dir =~ /(.+)( \/|\@ )\z/x) {
+    if ($dir=~ /\@$/) {
+        $dir=~ s/\@$//;
+        unless (-e $dir) {
+            $dir= "$dir/";
+        } elsif (-d $dir) {
+            $dir= "$dir/";
+        }
+    }
+
+    if ($dir =~ /(.+)(\/)\z/x) {
         my $base = $1;
-        $dir =~ s/@\z/\//;
         $pwd = "$pwd$base/";
-        main($pwd, $dir, $args, $fmt);
+        main($pwd, $dir, @args);
     }
     elsif ($dir =~ /$quit/) {
         out($pwd);
@@ -90,6 +80,10 @@ sub main {
 sub out {
     $pwd = shift;
     $pwd =~ s/@\z//;
+
+    if ($command eq 'open' && -f $pwd) {
+        print `$command $pwd`;
+    }
     print `echo $pwd`;
     exit;
 }
